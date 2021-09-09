1b8dfc48a32886243029e2101832611d4f3e961f
cenfv <carlosnfreitasv@gmail.com>
2021-09-09 18:19:37 -0300

    Group by search

--- backend/src/main/java/com/cenfv/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/cenfv/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.cenfv.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cenfv.dsvendas.dto.SaleDTO;
+import com.cenfv.dsvendas.dto.SaleSucessDTO;
+import com.cenfv.dsvendas.dto.SaleSumDTO;
 import com.cenfv.dsvendas.entities.Sale;
 import com.cenfv.dsvendas.repositories.SaleRepository;
 import com.cenfv.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,14 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	@Transactional(readOnly = true)
+	public List<SaleSucessDTO> sucessGroupedBySeller(){
+		return repository.sucessGroupedBySeller();
 	}
 }
